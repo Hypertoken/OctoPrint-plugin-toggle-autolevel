7b2a379a2a086c2f5e9b18c048fcccc1f9ca4fd8
myJS.io Support <info@myJS.io>
2017-09-18 18:13:20 -0700

    v1.0.1 Initial version

---
+++ ToggleAutolevel.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+import octoprint.plugin
+
+class ToggleAutolevelPlugin(octoprint.plugin.OctoPrintPlugin):
+	def rewrite_g29(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+		if gcode and gcode == "G29":
+			cmd = ";G29"
+		return cmd,
+
+	def sent_g29(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+		if gcode and gcode == ";G29":
+			self._logger.info("Just commented G29: {cmd}".format(**locals()))
+
+__plugin_name__ = "Toggle Autolevel"
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = ToggleAutolevelPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_g29,
+		"octoprint.comm.protocol.gcode.sent": __plugin_implementation__.sent_g29
+	}
+
